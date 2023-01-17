1957ab65c6df8db23be2747e1942536f0ff54e0b
h9661 <guscksdn123@gmail.com>
2023-01-17 13:05:43 +0900

    updated show.html to display each Recipe

--- src/main/java/chanwoo/recipe/project/domain/Recipe.java
+++ src/main/java/chanwoo/recipe/project/domain/Recipe.java
@@ -1,14 +1,9 @@
 package chanwoo.recipe.project.domain;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
-@Data
 @Entity
 public class Recipe {
 
@@ -44,6 +39,9 @@ public class Recipe {
             inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
+    public Recipe() {
+    }
+
     public void setNotes(Notes notes) {
         this.notes = notes;
         notes.setRecipe(this);
@@ -53,5 +51,105 @@ public class Recipe {
         ingredient.setRecipe(this);
         this.ingredients.add(ingredient);
         return this;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public Integer getPrepTime() {
+        return this.prepTime;
+    }
+
+    public Integer getCookTime() {
+        return this.cookTime;
+    }
+
+    public Integer getServings() {
+        return this.servings;
+    }
+
+    public String getSource() {
+        return this.source;
+    }
+
+    public String getUrl() {
+        return this.url;
+    }
+
+    public String getDirections() {
+        return this.directions;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return this.ingredients;
+    }
+
+    public Byte[] getImage() {
+        return this.image;
+    }
+
+    public Difficulty getDifficulty() {
+        return this.difficulty;
+    }
+
+    public Notes getNotes() {
+        return this.notes;
+    }
+
+    public Set<Category> getCategories() {
+        return this.categories;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrepTime(Integer prepTime) {
+        this.prepTime = prepTime;
+    }
+
+    public void setCookTime(Integer cookTime) {
+        this.cookTime = cookTime;
+    }
+
+    public void setServings(Integer servings) {
+        this.servings = servings;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setDirections(String directions) {
+        this.directions = directions;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void setImage(Byte[] image) {
+        this.image = image;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
