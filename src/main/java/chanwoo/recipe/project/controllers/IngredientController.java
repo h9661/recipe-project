628765ce2ad60deca6c8751f339ff82bb1dacc4c
h9661 <guscksdn123@gmail.com>
2023-01-19 14:23:31 +0900

    implemented view IngredientController and ingredient/list.html

---
+++ src/main/java/chanwoo/recipe/project/controllers/IngredientController.java
@@ -0,0 +1,27 @@
+package chanwoo.recipe.project.controllers;
+
+import chanwoo.recipe.project.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+public class IngredientController {
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    @RequestMapping("/recipe/{id}/ingredients")
+    public String listIngredients(@PathVariable String id, Model model){
+        model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
+
+        return "recipe/ingredient/list";
+    }
+}
